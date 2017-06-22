1eb6c80910ce8c52f05ef144eb8efece3acd2b76
DRPedit <jmarsico@gmail.com>
2017-06-22 14:49:37 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("start test program");
+var MotorSystem = require("dynanode/MotorSystem");
+
+var ms = new MotorSystem();
+ms.addToBlackList("/dev/tty.Bluetooth-PDA-Sync");
+ms.addToBlackList("/dev/tty.Bluetooth-Modem");
+
+ms.on("motorAdded",function(d) {
+	d.motor.on("valueUpdated",function(m){
+		console.log("value updated: "+d.motor.getID()+" "+m.name+":"+m.value);
+	});
+	console.log(d.motor.setRegisterValue("torqueEnable",1));
+	console.log(d.motor.setRegisterValue("movingSpeed",10));
+	console.log(d.motor.setRegisterValue("goalPosition",512));
+	console.log("motor added "+d.motor.getID());
+});
+
+ms.init();
+
+process.on('SIGINT', function() {
+	console.log("shutdown command");
+	ms.terminate();
+});
